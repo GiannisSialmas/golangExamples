2f38c1e80a9dedfd5432b92382408632d6032786
GiannisSialmas <giannissialmas@gmail.com>
2019-10-05 02:42:06 +0300

    Added database call code

--- chinook-orm/main.go
+++ chinook-orm/main.go
@@ -1,15 +1,13 @@
 package main
 
-
 // We are importing the package so that it can register its drivers with the database/sql package, and we use the _ identifier
 // to tell Go that we still want this included even though we will never directly reference the package in our code.
 import (
 	"database/sql"
 	"fmt"
 	"log"
-	
-	_ "github.com/lib/pq"
 
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -18,9 +16,9 @@ const (
 	user     = "postgres"
 	password = "chinook"
 	dbname   = "postgres"
-  )
+)
 
-func check(error interface{})  {
+func check(error interface{}) {
 	if error != nil {
 		log.Fatal(error)
 		panic(error)
@@ -29,16 +27,28 @@ func check(error interface{})  {
 
 func main() {
 
-	connStr := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable",host, port, user, password, dbname)
+	connStr := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", connStr)
 	check(err)
+	defer db.Close()
+
 	err = db.Ping()
 	check(err)
 	fmt.Println("Database ping succesfull")
 
-	// rows, err := db.QueryContext(ctx, "SELECT * FROM users WHERE age = $1", age)
-	// check(err)
-	// defer rows.Close()
+	rows, err := db.Query("SELECT employee_id,first_name FROM employee")
+	check(err)
+	defer rows.Close()
+	for rows.Next() {
+		var employee_id int
+		var first_name string
+		err = rows.Scan(&employee_id, &first_name)
+		if err != nil {
+			// handle this error
+			panic(err)
+		}
+		fmt.Println(employee_id, first_name)
+	}
 
 }
